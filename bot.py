01ce6bd93936743668bb37975b478a5803722708
Predator HackerzZ <77227205+P-Phreak@users.noreply.github.com>
2021-05-02 21:28:00 +0530

    bot.py

--- bot.py
+++ bot.py
@@ -27,7 +27,7 @@ if __name__ == "__main__" :
     plugins = dict(
         root="plugins"
     )
-    app = Predator("TeleRoidRENAMEBOT", 
+    app = Predator("TeleRoid_Renamer_Bot", 
           bot_token=Config.TG_BOT_TOKEN, 
           api_id=Config.APP_ID,
           api_hash=Config.API_HASH,
